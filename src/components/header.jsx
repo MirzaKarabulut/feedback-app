c425ff8d2fc22a5f8d32c281e7fd04a15bb71f6d
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-12 13:17:19 +0300

    chore: Update header component with prop types and default values

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,12 @@
-function header({ text}) {
+import PropTypes from 'prop-types'
+
+function Header({ text, bgColor, textColor }) {
+  const headerStyle = {
+    backgroundColor:bgColor,
+    color:textColor
+  }
   return (
-    <header>
+    <header style={headerStyle}>
       <div className="container">
         <h2>{ text}</h2>
       </div>
@@ -8,8 +14,16 @@ function header({ text}) {
   )
 }
 
-header.defaultProps = {
-  text: "Feedback UI"
+Header.defaultProps = {
+  text: "Feedback UI",
+  bgColor: 'rgba(0,0,0,0.4)', // Update property name to backgroundColor
+  textColor: '#ff6a95' // Update property name to color
 }
 
-export default header
+Header.propTypes = {
+  text: PropTypes.string,
+  backgroundColor: PropTypes.string, // Update property name to backgroundColor
+  color: PropTypes.string // Update property name to color
+}
+
+export default Header
