dd5f449c8a2e96660e1efbbcfdfcc4ef595da873
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-12 16:08:55 +0300

    chore: Update FeedbackItem component to include close button

--- src/components/feedbackItem.jsx
+++ src/components/feedbackItem.jsx
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types'
 function FeedbackItem({ item }) {
   return (
     <Card>
-      <div className="num-display">{ item.rating}</div>
+      <div className="num-display">{item.rating}</div>
+      <button className="close">
+      </button>
         <div className="text-display">
           {item.text}
       </div>
