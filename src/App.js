79ff6fba46c3769e176693f7f623396cfbaf564f
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-13 23:37:47 +0300

    chore: Add deleteFeedback function to handle deleting feedback items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
 import Header from "./components/header";
 import FeedbackList from "./components/feedbackList";
 import { useState } from "react";
 import feedbackData from "./data/feedbackData";
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
+  const deleteFeedback = (id) => { 
+    console.log("app", id);
+  }
   return (
     <>
     < Header />
     <div className="container">
-      < FeedbackList feedback = {feedback} />
+      < FeedbackList feedback = {feedback} handleDelete={deleteFeedback} />
     </div>
     </>
   );
