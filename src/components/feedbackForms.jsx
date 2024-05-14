d5daf3ec1a88c6196336dbec9a618b47768453b8
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-14 16:58:21 +0300

    refactor: Refactor FeedbackForms component for improved code structure and readability

--- src/components/feedbackForms.jsx
+++ src/components/feedbackForms.jsx
@@ -1,36 +1,45 @@
-import React, { useState } from 'react'
-import Card from './shared/Card'
-import Button from './shared/button'
+import React from "react";  
+import { useState } from 'react';
+import Card from "./shared/Card";
+import Button from "./shared/button";
+import RatingSelect from "./ratingSelect";
 
-function feedbackForms() {
-  const [text, setText] = useState('')
-  const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] = useState('hello')
+function FeedbackForms() {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("hello");
 
   const handleTextChange = (e) => {
-    if (text === '') {
-      setBtnDisabled(true)
-      setMessage(null)
-    } else if (text !== '' && text.trim().length <= 10) {
-      setBtnDisabled(true)
-      setMessage('At least 10 characters required')
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("At least 10 characters required");
     } else {
-      setBtnDisabled(false)
-      setMessage(null)
+      setBtnDisabled(false);
+      setMessage(null);
     }
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
   return (
     <Card>
       <h2>Can you share your feedback about your service with us?</h2>
+      <RatingSelect select={(rating) => console.log(rating)} />
       <div className="input-group">
-        <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
-        <Button type='submit' isDisabled = {btnDisabled}>Send</Button>
+        <input
+          onChange={handleTextChange}
+          type="text"
+          placeholder="Write a review"
+          value={text}
+        />
+        <Button type="submit" isDisabled={btnDisabled}>
+          Send
+        </Button>
       </div>
       {message && <div className="message">{message}</div>}
     </Card>
-  
-  )
+  );
 }
 
-export default feedbackForms
+export default FeedbackForms;
