0b90f4b00b02aeb6fbf1e6019c2b4b755f854e1c
mirzakarabulut <mirzakara1234@gmail.com>
2024-05-14 16:09:52 +0300

    feat: Create FeedbackForms component for user feedback submission

---
+++ src/components/feedbackForms.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import Card from './shared/Card'
+import Button from './shared/button'
+
+function feedbackForms() {
+  const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('hello')
+
+  const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('At least 10 characters required')
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+    setText(e.target.value)
+  }
+  return (
+    <Card>
+      <h2>Can you share your feedback about your service with us?</h2>
+      <div className="input-group">
+        <input onChange={handleTextChange} type="text" placeholder='Write a review' value={text} />
+        <Button type='submit' isDisabled = {btnDisabled}>Send</Button>
+      </div>
+      {message && <div className="message">{message}</div>}
+    </Card>
+  
+  )
+}
+
+export default feedbackForms
